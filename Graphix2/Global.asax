968c095b86d25f9c4da6155c0edcad97e1171bc3
vmuser <vmuser@VM>
2017-05-23 20:03:53 +0000

    First Commit

---
+++ Graphix2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Graphix2.Global" Language="C#" %>
---
+++ Graphix2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Graphix2.Startup))]
+namespace Graphix2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
